6e43aad26f50166df35bafdfe87bd51b4f6dd249
awais-developer-coder <emmanishaq610@gmail.com>
2025-02-15 13:44:21 +0000

    new work

---
+++ class 10/app.js
@@ -0,0 +1,43 @@
+// let num=5;
+// let userInput = prompt("Enter a Number")
+
+// //  let random =(Math.floor(Math.random() * 11))
+//  if (num== userInput ){
+//     document.write("congratulate your number ")
+
+//  }
+//  else{
+//     document.write("try again")
+//  }
+
+
+
+// let player = prompt("Enter your Favourite Player")
+// let player2 = prompt("Enter your Favourite Player")
+
+
+
+// let toss = (Math.floor(Math.random() *2))
+
+// if(toss==0){
+//     console.log(player + ": win the toss")
+// }
+// else{
+//     console.log(player2 + ": win the toss")
+// }
+
+
+// let random = Math.floor(Math.random() *12345678)
+// console.log(random)
+
+
+
+
+let str = "abcdefghijklimnopqrstuvwxyz1234567890!@#$%^&*()_-+|}{"
+let check =""
+let length= 8
+
+for( var i=0; i<length; i++){
+    check+= str[Math.floor(Math.random()*str.length) ]
+}
+console.log(check)
